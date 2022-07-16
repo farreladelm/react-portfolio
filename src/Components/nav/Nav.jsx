5110ba445917335e77efc2f373f2648076543866
Farrel Mohammad <farrel.adel@gmail.com>
2022-07-16 22:28:08 +0700

    update link nav

--- src/Components/nav/Nav.jsx
+++ src/Components/nav/Nav.jsx
@@ -11,7 +11,7 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
    <nav>
-    <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeAlt/></a>
+    <a href="#home" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeAlt/></a>
     <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active': ''}><AiOutlineUser/></a>
     <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active': ''}><BsBookmarkStar/></a>
     <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active': ''}><ImBook/></a>
