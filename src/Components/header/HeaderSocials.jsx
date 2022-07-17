a594f795b832e0f105a641856e59c8c7dab05e13
Farrel Mohammad <farrel.adel@gmail.com>
2022-07-17 13:47:30 +0700

    update alert after send message

--- src/Components/header/HeaderSocials.jsx
+++ src/Components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ import {BsFacebook} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://www.linkedin.com/in/farrel-adel-mohammad-566026179/" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://www.linkedin.com/in/farreladelm/" target="_blank" rel="noreferrer"><BsLinkedin/></a>
         <a href="https://github.com/farreladelm" target="_blank" rel="noreferrer"><BsGithub/></a>
         <a href="https://www.facebook.com/farreladelm/" target="_blank" rel="noreferrer"><BsFacebook/></a>
     </div>
